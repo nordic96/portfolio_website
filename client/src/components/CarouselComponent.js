c6978a0c39eb5dc57851443fa0b782794a29785d
Gi Hun Ko <rhrlgns96@gmail.com>
2020-11-22 16:09:51 +0800

    Closes #1

--- client/src/components/CarouselComponent.js
+++ client/src/components/CarouselComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Slide from 'react-reveal/Slide';
 import makeCarousel from 'react-reveal/makeCarousel';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default class CarouselComponent extends Component {
     constructor(props) {
@@ -30,33 +30,60 @@ export default class CarouselComponent extends Component {
       }
     
     componentDidMount() {
-        this.fetchProjects();
+      this.fetchProjects();
     }
     render() {
-        const Container = styled.div`
-        margin: auto;
-        position: relative;
-        overflow: hidden;
-        width: 90%;
-        height: 50vh;
-        `;
-        const CarouselUI = ({ children }) => <Container>{children}</Container>;
-        const Carousel = makeCarousel(CarouselUI);
-        return (
-        <Carousel>
-            {this.state.projects.map((project) => {
-                return(
-                    <Slide right>
-                        <div>
-                            <img className="img-proj-card" src={project.medialink}/>
-                            <h2>{project.name} ({project.devyear})</h2> 
-                            <div className="desc">
-                                {project.desc}
-                            </div>
-                        </div>
-                    </Slide>
-                )
-            })}
+      const width = "100%", height = "44vh";
+      const Container = styled.div`
+      margin: auto;
+      position: relative;
+      overflow: hidden;
+      width: ${width};
+      height: ${height};
+      `;
+      const Children  = styled.div`
+      width: ${width};
+      position: relative;
+      height: ${height};
+      `;
+      const Arrow = styled.div`
+      text-shadow: 1px 1px 1px #fff;
+      z-index: 100;
+      line-height: ${height};
+      text-align: center;
+      position: absolute;
+      top: 0;
+      width: 10%;
+      font-size: 3em;
+      cursor: pointer;
+      user-select: none;
+      ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
+      `;
+      const CarouselUI = ({ position, handleClick, children }) => (
+        <Container>
+          <Children>
+            {children}
+            <Arrow onClick={handleClick} data-position={position - 1}>{'<'}</Arrow>
+            <Arrow right onClick={handleClick} data-position={position + 1}>{'>'}</Arrow>
+          </Children>
+        </Container>
+      );
+      const Carousel = makeCarousel(CarouselUI);
+      return (
+      <Carousel>
+          {this.state.projects.map((project) => {
+              return(
+                  <Slide right>
+                      <div className="proj-card">
+                          <img className="img-proj-card" src={project.medialink}/>
+                          <h2>{project.name} ({project.devyear})</h2> 
+                          <div className="desc">
+                              {project.desc}
+                          </div>
+                      </div>
+                  </Slide>
+              )
+          })}
         </Carousel>
         );
     };
