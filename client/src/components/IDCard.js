eca111fa09b093c5c00f73e24422d1a9019f2ec0
Gi Hun Ko <rhrlgns96@gmail.com>
2020-11-29 23:31:17 +0800

    implemented bootstrap with other css changes

--- client/src/components/IDCard.js
+++ client/src/components/IDCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../logo.svg';
 
 export default class IDCard extends Component {
     constructor(props) {
@@ -10,7 +11,9 @@ export default class IDCard extends Component {
             <div>
                 {/* <img className="id-lanyard" src="assets/lanyard.png" alt="lanyard"/> */}
                 <div className="id-card">
-                    <div className="id-black"></div>
+                    <div className="id-black">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </div>
                     <div className="id-maroon"></div>
                     <div className="id-skyblue"></div>
                     <div className="id-content">
@@ -18,20 +21,20 @@ export default class IDCard extends Component {
                         <img className="intro-selfie" src='assets/selfie_2.png' alt="profile"/>
                         <br/>
                         <b>GI HUN KO</b><br/>
-                        <b>Software Engineer</b><br/>
-                        <b>Singapore</b><br/>
-                        <div className="id-content-gray">
-                            <a href="https://github.com/nordic96"><img className="link-icon" src="assets/github-logo.png" alt="github icon"/></a>
-                            &nbsp;<a href="https://www.linkedin.com/in/gi-hun-ko-863619184/"><img className="link-icon" src="assets/Linkedin-Icon.png" alt="linkedin icon"/></a>
-                            &nbsp;<a href="https://www.hackerrank.com/kogihun"><img className="link-icon" src="assets/hackerrank.svg" alt="hackerrank icon"/></a>
-                        </div>
+                        <b>Software Engineer, Singapore</b><br/>
+                        <a href="https://github.com/nordic96"><img className="link-icon" src="assets/github-logo.png" alt="github icon"/></a>
+                        &nbsp;<a href="https://www.linkedin.com/in/gi-hun-ko-863619184/"><img className="link-icon" src="assets/Linkedin-Icon.png" alt="linkedin icon"/></a>
+                        &nbsp;<a href="https://www.hackerrank.com/kogihun"><img className="link-icon" src="assets/hackerrank.svg" alt="hackerrank icon"/></a>
+                        <br/><img className="id-barcode" src="assets/barcode.png" />
+                        {/* <div className="id-content-gray">
+                        </div> */}
                     </div>
                     <div className="id-skyblue"></div>
                     <div className="id-maroon"></div>
                     <div className="id-black"></div>
                 </div>
                 <div className="intro-desc">
-                    <h2>Ko Gi Hun's Portfolio Website</h2>
+                    <h1><b>Ko Gi Hun's Portfolio Website</b></h1>
                     <p>
                     "I am a Korean Computer Science student at NUS. 
                     My career goal is to become an expert in software engineering, as well as able to work along with fantastic people to design a software that is easily interactable, with simple, but attractive design. 
