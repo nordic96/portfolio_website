74e2f444f63c2dee5a790a74d70c7405f19ac3cf
Gi Hun Ko <rhrlgns96@gmail.com>
2020-12-12 14:22:29 +0800

    implemented bootstrap popper for animation, as well as changing layout with added navbar on top

--- client/src/App.js
+++ client/src/App.js
@@ -60,9 +60,30 @@ class App extends Component {
     return (
       <div className="App">
         <div className="header-top">
-          &nbsp;
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Ko Gi Hun's Portfolio Website</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                {/* <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#">Disabled</a>
+                </li> */}
+              </ul>
+            </div>
+          </nav>
         </div>
-        <header className="App-header">
+        <div className="container-intro">
           <div className="intro">
             <IDCard />
             <div>
@@ -77,55 +98,65 @@ class App extends Component {
               />
             </div>
           </div>
-        </header>
-        <div id="content">
-          <div id="menu-header">
-            <b>&lt; My Projects /&gt;</b>
-          </div>
-          <div className="project-list">
-            {/* <CarouselComponent /> */}
-              {this.state.projects.map((project) => {
-                return(
-                  <ProjectCardComponent 
-                  name={project.name} 
-                  medialink={project.medialink} 
-                  projectlink={project.projectlink}
-                  videolink={project.videolink}
-                  projecttype={project.projecttype} 
-                  tags={project.tags}
-                  devyear={project.devyear}
-                  desc={project.desc}
-                  />
-                )
-              })}
-            <CardColumns>
-            </CardColumns> 
-          </div>
-          <div className="container-design">
-            <div id="menu-header">
-              <b>&lt; Design Works /&gt;</b>
-            </div>
-            <div className="desc-main">
-              <p>Not only just coding, but I also have background experience in visual designs using illustration tools with group members when I was a member of publicity teams in my CCA organisations.
-                Below works are some of the designs that I have worked on :)</p>                
-            </div>
-            <div>
-              <CardColumns>
-                {this.state.designs.map((design) => {
-                  return (
-                    <CardComponent 
-                    medialink={design.medialink} 
-                    name={design.name} 
-                    desc={design.desc} 
-                    org={design.organisation} 
-                    year={design.year}
-                    />
-                  );
-                })}
-              </CardColumns>
-            </div>
-          </div>
         </div>
+        <div className="container-fluid">
+          <div className="container-intro">
+          </div>
+          <div className="content">
+            {/* <div id="menu-header">
+              <b>&lt; My Projects /&gt;</b>
+            </div> */}
+            <div className="item-intro">
+              <h2 className="display-4"><b>Projects</b></h2>
+              <p className="lead">My projects include school projects, as well as self-sourced projects, either in groups or single. Project scope varies from IoT, machine learning, to Software Engineering!</p> 
+              <hr className="my-4"/>
+              <p>All of the project source files are hosted in github or displayed in youtube, and you can click the github/youtube icon to see more details about it.</p> 
+            </div>
+            <div className="project-list">
+              {/* <CarouselComponent /> */}
+                {this.state.projects.map((project) => {
+                  return(
+                    <ProjectCardComponent 
+                    name={project.name} 
+                    medialink={project.medialink} 
+                    projectlink={project.projectlink}
+                    videolink={project.videolink}
+                    projecttype={project.projecttype} 
+                    tags={project.tags}
+                    devyear={project.devyear}
+                    desc={project.desc}
+                    />
+                  )
+                })}
+              <CardColumns>
+              </CardColumns> 
+            </div>
+            <div className="item-intro">
+              <h2 className="display-4"><b>Design Works</b></h2>
+              <p className="lead">Not only just coding, but I also have background experience in visual designs using illustration tools with group members when I was a member of publicity teams in my CCA organisations.
+                Below works are some of the designs that I have worked on :)</p> 
+              <hr className="my-4"/>
+              <p></p> 
+            </div>
+            <div className="container-design">
+              <div>
+                <CardColumns>
+                  {this.state.designs.map((design) => {
+                    return (
+                      <CardComponent 
+                      medialink={design.medialink} 
+                      name={design.name} 
+                      desc={design.desc} 
+                      org={design.organisation} 
+                      year={design.year}
+                      />
+                    );
+                  })}
+                </CardColumns>
+              </div>
+            </div>
+          </div>
+        </div> 
         <FooterComponent />
       </div>
     );
