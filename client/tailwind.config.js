bb45209267df1bed7df38e789374c479bf0f6719
nordic96 <rhrlgns96@gmail.com>
2022-10-21 17:38:49 +0800

    added new tailwind color config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                hotpink: '#fa256d',
+                velvet: '#79096d',
+            },
+        },
     },
     plugins: [],
 };
