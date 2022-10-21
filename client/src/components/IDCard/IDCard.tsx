45b14c2a3e157eea0b5fd39a259c7d3f1e7579a3
nordic96 <rhrlgns96@gmail.com>
2022-10-21 16:04:55 +0800

    added gradiant and new profile pic

--- client/src/components/IDCard/IDCard.tsx
+++ client/src/components/IDCard/IDCard.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 const IDCard = () => {
     return (
-        <div className={''}>
-            <img
-                className={'h-96'}
-                src={'assets/selfie.jpeg'}
-                alt={'profile'}
-            />
+        <div className={'relative'}>
+            <div
+                className={
+                    '-top-7 left-5 w-80 h-80 bg-gradient-to-r from-indigo-600 to-cyan-600 rounded-full -z-1 absolute'
+                }>
+                &nbsp;
+            </div>
+            <div className={'z-1 relative left-12 top-7'}>
+                <img
+                    className={'h-80'}
+                    src={'assets/selfie.png'}
+                    alt={'profile'}
+                />
+            </div>
         </div>
     );
 };
