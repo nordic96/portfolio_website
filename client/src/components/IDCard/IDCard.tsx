ccdcdca02383bfd5a444232a9e2c2759893cbb1f
nordic96 <rhrlgns96@gmail.com>
2022-10-21 15:37:24 +0800

    refactored intro section comp

--- client/src/components/IDCard/IDCard.tsx
+++ client/src/components/IDCard/IDCard.tsx
@@ -1,45 +1,13 @@
 import React from 'react';
 
-import Logo from '../../assets/images/logo.svg';
-import createStyles from './styles';
-import LabelContainer from 'labelcontainer';
-import { Icons } from '../../constants/profileIcons';
-import IDIcon from '../IDIcon/IDIcon';
-
 const IDCard = () => {
-    const classes = createStyles();
-    const labelInstance = LabelContainer.getInstance();
     return (
-        <div className={classes.container}>
-            <div className={classes.cardContainer}>
-                <div className="id-black">
-                    <img src={Logo} className="App-logo" alt="logo" />
-                </div>
-                <div className="id-maroon"></div>
-                <div className="id-skyblue"></div>
-                <div className="id-content">
-                    <b>NUS Computer Science</b>
-                    <br />
-                    <img
-                        className="intro-selfie"
-                        src="assets/selfie_2.png"
-                        alt="profile"
-                    />
-                    <br />
-                    <b>GI HUN KO</b>
-                    <br />
-                    <b>Software Engineer, Singapore</b>
-                    <br />
-                    <img
-                        className="id-barcode"
-                        src="assets/barcode.png"
-                        alt="barcode"
-                    />
-                </div>
-                <div className="id-skyblue"></div>
-                <div className="id-maroon"></div>
-                <div className="id-black"></div>
-            </div>
+        <div className={''}>
+            <img
+                className={'h-96'}
+                src={'assets/selfie.jpeg'}
+                alt={'profile'}
+            />
         </div>
     );
 };
--- client/src/components/IntroSection/IntroSection.tsx
+++ client/src/components/IntroSection/IntroSection.tsx
@@ -1,5 +1,6 @@
-import LabelContainer from 'labelcontainer';
 import React from 'react';
+import { Grow } from '@mui/material';
+import LabelContainer from 'labelcontainer';
 
 import Wave from 'react-wavify';
 import { Icons } from '../../constants/profileIcons';
@@ -9,39 +10,52 @@ import IDIcon from '../IDIcon/IDIcon';
 const IntroSection = () => {
     const lsInstance = LabelContainer.getInstance();
     return (
-        <div className={'flex bg-white flex-1 row'}>
+        <div className={'block bg-white'}>
             <div
                 className={
                     'flex flex-1 flex-row pt-20 justify-center items-start gap-10'
                 }>
-                <div className={'flex flex-column items-start'}>
-                    <p className={'text-4xl font-bold'}>
-                        {lsInstance.getLabel('title')}
-                    </p>
-                    <p className={'text-2xl'}>
-                        {lsInstance.getLabel('title_desc')}
-                    </p>
-                    <p className={'text-base'}>
-                        {lsInstance.getLabel('intro_desc')}
-                    </p>
-                    <div className={'flex flex-row gap-2 pt-2'}>
-                        {Icons.map((x, i) => {
-                            return <IDIcon {...x} key={i} />;
-                        })}
+                <Grow in timeout={1000}>
+                    <div className={'flex flex-column items-start'}>
+                        <p className={'text-4xl font-bold'}>
+                            {lsInstance.getLabel('title')}&#9995;
+                        </p>
+                        <p className={'text-2xl'}>
+                            {lsInstance.getLabel('title_desc')}
+                        </p>
+                        <p className={'text-base'}>
+                            {lsInstance.getLabel('intro_desc')}
+                        </p>
+                        <div className={'flex flex-row gap-2 pt-2'}>
+                            {Icons.map((x, i) => {
+                                return <IDIcon {...x} key={i} />;
+                            })}
+                        </div>
                     </div>
-                </div>
+                </Grow>
                 <IDCard />
             </div>
-            <Wave
-                fill="#9c28d3"
-                paused={false}
-                options={{
-                    height: 70,
-                    amplitude: 40,
-                    speed: 0.22,
-                    points: 5,
-                }}
-            />
+            <div className={'-top-20 flex relative'}>
+                <Wave
+                    fill={'url(#gradient)'}
+                    paused={false}
+                    options={{
+                        height: 75,
+                        amplitude: 35,
+                        speed: 0.22,
+                        points: 5,
+                    }}>
+                    <defs>
+                        <linearGradient
+                            id="gradient"
+                            gradientTransform="rotate(0)">
+                            <stop offset="0%" stopColor="#fa256d" />
+                            <stop offset="35%" stopColor="#79096d" />
+                            <stop offset="90%" stopColor="#0070ff" />
+                        </linearGradient>
+                    </defs>
+                </Wave>
+            </div>
         </div>
     );
 };
