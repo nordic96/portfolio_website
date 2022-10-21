b6976722b0374b4aad0a6c476243a47636e368c8
nordic96 <rhrlgns96@gmail.com>
2022-10-22 00:48:37 +0800

    refactored design section

---
+++ client/src/components/DesignSection/DesignSection.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
+import LabelContainer from 'labelcontainer';
+import DesignCard from '../DesignCard';
+import { CardColumns } from 'react-bootstrap';
+import { FullDesignDesc } from '../../globals';
+
+const DesignSection = () => {
+    const labelInstance = LabelContainer.getInstance();
+    const [designs, setDesigns] = useState<Array<FullDesignDesc>>([]);
+    const [isDesignsLoaded, setIsDesignsLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        fetch('/api/fetchDesigns')
+            .then((res) => res.json())
+            .then(
+                (designs) => {
+                    setDesigns(designs);
+                    setIsDesignsLoaded(true);
+                },
+                (ex) => {
+                    console.log('Fetch failed, ', ex);
+                    setIsDesignsLoaded(false);
+                }
+            );
+    }, []);
+
+    return (
+        <div>
+            <hr className="my-4" />
+            <div
+                className={'flex items-center justify-center py-4 flex-column'}>
+                <p className={'text-2xl w-50 text-black italic'}>
+                    {`"${labelInstance.getLabel('cca_msg')}"`}
+                </p>
+            </div>
+            <div className="container-design">
+                <div>
+                    <CardColumns>
+                        {isDesignsLoaded
+                            ? designs.map(
+                                  (design: FullDesignDesc, index: number) => {
+                                      return (
+                                          <DesignCard
+                                              key={index}
+                                              designProject={design}
+                                          />
+                                      );
+                                  }
+                              )
+                            : "Design details fetch request failed :'( please refresh the page again!"}
+                    </CardColumns>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DesignSection;
