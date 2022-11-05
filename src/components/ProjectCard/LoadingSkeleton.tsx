72a0f02fcad288ec298c0f2d62490813eefce4f6
nordic96 <rhrlgns96@gmail.com>
2022-11-05 21:18:57 +0800

    refactored loading skeleton

--- src/components/ProjectCard/LoadingSkeleton.tsx
+++ src/components/ProjectCard/LoadingSkeleton.tsx
@@ -1,28 +1,12 @@
-import { useTheme } from 'next-themes';
 import React from 'react';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from '../SkeletonComp';
 
 const LoadingSkeleton = () => {
-    const { systemTheme } = useTheme();
-    console.log(systemTheme);
     return (
         <div className={'w-1/4 max-sm:w-full flex flex-column text-left'}>
-            <Skeleton
-                height={200}
-                baseColor={systemTheme === 'dark' ? '#24292F' : undefined}
-                highlightColor={systemTheme === 'dark' ? '#383C42' : undefined}
-            />
-            <Skeleton
-                width={'70%'}
-                baseColor={systemTheme === 'dark' ? '#24292F' : undefined}
-                highlightColor={systemTheme === 'dark' ? '#383C42' : undefined}
-            />
-            <Skeleton
-                width={'50%'}
-                baseColor={systemTheme === 'dark' ? '#24292F' : undefined}
-                highlightColor={systemTheme === 'dark' ? '#383C42' : undefined}
-            />
+            <Skeleton height={200} />
+            <Skeleton width={'70%'} />
+            <Skeleton width={'50%'} />
         </div>
     );
 };
