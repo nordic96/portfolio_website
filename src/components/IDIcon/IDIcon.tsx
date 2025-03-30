cca983990c3384ab0d600a7f1e9ece17f015ee74
rhrlgns96 <rhrlgns96@gmail.com>
2025-03-30 04:41:27 +0000

    refactored IDIcon Component to accept custom class names

--- src/components/IDIcon/IDIcon.tsx
+++ src/components/IDIcon/IDIcon.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 export interface IDIconProps {
     href: string;
     src: string;
+    className?: string;
 }
 
 const IDIcon = (props: IDIconProps) => {
-    const { href, src } = props;
+    const { href, src, className } = props;
     return (
         <a href={href} target="_blank" rel="noreferrer">
-            <img className="link-icon" src={src} alt={src} />
+            <img className={className ?? "link-icon"} src={src} alt={src} />
         </a>
     );
 };
