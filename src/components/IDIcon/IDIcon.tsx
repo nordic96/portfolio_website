4dc59895cf5aeeae66e67fd0f0803e9b208fa5a4
rhrlgns96 <rhrlgns96@gmail.com>
2025-03-31 07:37:12 +0000

    added bouncing effects on certificate section

--- src/components/IDIcon/IDIcon.tsx
+++ src/components/IDIcon/IDIcon.tsx
@@ -4,14 +4,17 @@ export interface IDIconProps {
     href: string;
     src: string;
     className?: string;
+    spanClassName?: string;
 }
 
 const IDIcon = (props: IDIconProps) => {
-    const { href, src, className } = props;
+    const { href, src, className, spanClassName } = props;
     return (
-        <a href={href} target="_blank" rel="noreferrer">
-            <img className={className ?? 'link-icon'} src={src} alt={src} />
-        </a>
+        <span className={spanClassName}>
+            <a href={href} target="_blank" rel="noreferrer">
+                <img className={className ?? 'link-icon'} src={src} alt={src} />
+            </a>
+        </span>
     );
 };
 
--- src/constants/profileIcons.ts
+++ src/constants/profileIcons.ts
@@ -26,11 +26,13 @@ export const generateIcons = (): IDIconProps[] => {
 export const generateCertificateIcons = (): IDIconProps[] => {
     return [
         {
+            spanClassName: 'animate-bounce',
             className: 'cert-icon',
             src: 'https://images.credly.com/size/340x340/images/0e284c3f-5164-4b21-8660-0d84737941bc/image.png',
             href: 'https://www.credly.com/badges/5a00ab1a-d116-4fe6-aad8-52105d3b0603',
         },
         {
+            spanClassName: 'animate-bounce',
             className: 'cert-icon',
             src: 'https://images.credly.com/size/340x340/images/1e6611ca-8afe-4ecc-ad4d-305fba52ee7e/1_LFCS-600x600.png',
             href: 'https://www.credly.com/badges/4176e5cc-c892-4af0-b4da-59c056bb5aa6',
