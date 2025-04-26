fb229b9d3922351f055a57107f1f9794486a6ecb
rhrlgns96 <rhrlgns96@gmail.com>
2025-04-26 06:17:34 +0000

    set up jotai for project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,60 +7,73 @@ import NavBar from '../components/NavBar';
 import FooterComp from '../components/FooterComp';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import { Provider } from 'jotai';
+
 import reportWebVitals from '../reportWebVitals';
 import { AppProps } from 'next/dist/shared/lib/router/router';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <div className={'App'}>
-            <Head>
-                <title>StephenKo&apos;s Portfolio</title>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="theme-color" content="#000000" />
-                <meta name="title" content="StephenKo's Portfolio" />
-                <meta
-                    name="description"
-                    content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
-                />
-                {/** OpenGraph Meta Data */}
-                <meta property="og:title" content="StephenKo's Portfolio" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://www.stephenghk.com/" />
-                <meta
-                    property="og:description"
-                    content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
-                />
-                <meta
-                    property="og:image"
-                    content="https://stephenghk.com/assets/thumbnail.png"
-                />
-                {/* Twitter Meta Data */}
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://metatags.io/" />
-                <meta
-                    property="twitter:title"
-                    content="StephenKo's Portfolio"
-                />
-                <meta
-                    property="twitter:description"
-                    content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
-                />
-                <meta
-                    property="twitter:image"
-                    content="https://stephenghk.com/assets/thumbnail.png"
-                />
-            </Head>
-            <CssBaseline />
-            <ThemeProvider attribute={'class'}>
-                <NavBar />
-                <Component {...pageProps} />
-                <FooterComp />
-            </ThemeProvider>
-        </div>
+        <Provider>
+            <div className={'App'}>
+                <Head>
+                    <title>StephenKo&apos;s Portfolio</title>
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    <meta name="theme-color" content="#000000" />
+                    <meta name="title" content="StephenKo's Portfolio" />
+                    <meta
+                        name="description"
+                        content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
+                    />
+                    {/** OpenGraph Meta Data */}
+                    <meta property="og:title" content="StephenKo's Portfolio" />
+                    <meta property="og:type" content="website" />
+                    <meta
+                        property="og:url"
+                        content="https://www.stephenghk.com/"
+                    />
+                    <meta
+                        property="og:description"
+                        content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
+                    />
+                    <meta
+                        property="og:image"
+                        content="https://stephenghk.com/assets/thumbnail.png"
+                    />
+                    {/* Twitter Meta Data */}
+                    <meta
+                        property="twitter:card"
+                        content="summary_large_image"
+                    />
+                    <meta
+                        property="twitter:url"
+                        content="https://metatags.io/"
+                    />
+                    <meta
+                        property="twitter:title"
+                        content="StephenKo's Portfolio"
+                    />
+                    <meta
+                        property="twitter:description"
+                        content="StephenKo's personal portfolio website to showcase software engineering & AI related projects"
+                    />
+                    <meta
+                        property="twitter:image"
+                        content="https://stephenghk.com/assets/thumbnail.png"
+                    />
+                </Head>
+                <CssBaseline />
+                <ThemeProvider attribute={'class'}>
+                    <NavBar />
+                    <Component {...pageProps} />
+                    <FooterComp />
+                </ThemeProvider>
+            </div>
+        </Provider>
     );
 }
 
