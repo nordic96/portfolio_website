440d91d87ed1db0604d5d8389794c4f49fd67903
rhrlgns96 <rhrlgns96@gmail.com>
2025-05-05 05:49:01 +0000

    refactored projects atom to use constructapiroute method

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export async function middleware(request: NextRequest) {
         logger.error(`[ERR RETRIEVING JSON BODY FROM NEXT RESPONSE] ${error}`);
         data = responseClone.text();
     }
-    logger.info(`[RESPONSE CODE: ${response.status} ${request.url}] ${data}`);
+    logger.info(`[RESPONSE CODE: ${response.status} ${request.url}] ${JSON.stringify(data)}`);
     return response;
 }
 
