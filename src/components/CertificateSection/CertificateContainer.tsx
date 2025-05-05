c01db00a3dc9eb00fb1792644b31d978f8442cc2
rhrlgns96 <rhrlgns96@gmail.com>
2025-05-05 06:01:43 +0000

    implemented loading

--- src/components/CertificateSection/CertificateContainer.tsx
+++ src/components/CertificateSection/CertificateContainer.tsx
@@ -2,18 +2,27 @@
 import React, { useEffect } from 'react';
 import CertBadge from './components/CertBadge';
 import { useAtom } from 'jotai';
-import { asyncFetchCertificateAtom, readonlyCertificateAtom } from '../../store/certificateAtom';
+import {
+    asyncFetchCertificateAtom,
+    readonlyCertificateAtom,
+} from '../../store/certificateAtom';
+import { readOnlyLoadingAtom } from '../../store/loadingAtom';
+import withLoadingSkeleton from '../../utils/withLoadingSkeleton';
+import LoadingSkeleton from './components/LoadingSkeleton';
 
 const CertificateContainer = () => {
-    const [,fetchCertificates] = useAtom(asyncFetchCertificateAtom);
+    const [, fetchCertificates] = useAtom(asyncFetchCertificateAtom);
     const [certificates] = useAtom(readonlyCertificateAtom);
+    const [loading] = useAtom(readOnlyLoadingAtom);
 
     useEffect(() => {
         fetchCertificates();
     }, []);
 
-    return(
+    return (
         <div className="flex flex-row gap-2 h-auto">
+            {(loading['certifications'] || certificates.length <= 0) &&
+                withLoadingSkeleton(LoadingSkeleton)(3)}
             {certificates.map((x, i) => {
                 return <CertBadge {...x} key={i} />;
             })}
@@ -22,4 +31,3 @@ const CertificateContainer = () => {
 };
 
 export default CertificateContainer;
-
--- src/store/certificateAtom.ts
+++ src/store/certificateAtom.ts
@@ -1,16 +1,19 @@
 import { atom } from 'jotai';
 import { ICertificate } from '../models/certification';
 import { constructApiRoute } from '../constants/routes';
+import { setLoadingValAtom } from './loadingAtom';
 
 const CertificateAtom = atom<ICertificate[]>([]);
 const readonlyCertificateAtom = atom((get) => get(CertificateAtom));
 
 const asyncFetchCertificateAtom = atom(null, async (get, set) => {
     const route = constructApiRoute('certifications');
+    set(setLoadingValAtom, 'certifications', true);
     const json = await fetch(route).then((res) => {
         return res.json();
     });
     set(CertificateAtom, json.data || []);
+    set(setLoadingValAtom, 'certifications', false);
 });
 
 export { readonlyCertificateAtom, asyncFetchCertificateAtom };
