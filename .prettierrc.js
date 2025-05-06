d1aae197f686102b866826844fa91dfa2dadea86
rhrlgns96 <rhrlgns96@gmail.com>
2025-05-06 04:01:46 +0000

    setup jest and testing environment

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    trailingComma: "es5",
+    trailingComma: 'es5',
     tabWidth: 4,
     singleQuote: true,
     //arrowParens: 'avoid',
     printWidth: 80,
     bracketSpacing: true,
     jsxBracketSameLine: true,
-}
+};
