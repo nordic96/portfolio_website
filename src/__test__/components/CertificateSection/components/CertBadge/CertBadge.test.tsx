fa3e5d277cf221e592aaa61a14244fd4691c2c75
rhrlgns96 <rhrlgns96@gmail.com>
2025-05-06 05:10:41 +0000

    added test for certbadge component

---
+++ src/__test__/components/CertificateSection/components/CertBadge/CertBadge.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CertBadge from '../../../../../components/CertificateSection/components/CertBadge';
+
+describe('CertBadge Component', () => {
+    it('should render cert badge component with correct labels', () => {
+        render(
+            <CertBadge
+                logo_src="/test"
+                name="name_test"
+                theme_color="violet"
+                credentials_url="url_test"
+                year_obtained="test_2025"
+            />
+        );
+        expect(screen.getByText('name_test')).toBeInTheDocument();
+        expect(screen.getByTestId('credentials_url')).toHaveAttribute(
+            'href',
+            'url_test'
+        );
+        expect(screen.getByText('test_2025')).toBeInTheDocument();
+        const divComponent = screen.getByTestId('badge_container');
+        expect(divComponent.classList).toContain('shadow-violet-500/50');
+    });
+});
