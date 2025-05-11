fecf5b970c01414ed8e71ce4352fddb8bce4f8de
rhrlgns96 <rhrlgns96@gmail.com>
2025-05-11 04:30:48 +0000

    unboarded mongoose and refactored new schema

--- src/models/configs.ts
+++ src/models/configs.ts
@@ -1,41 +1,10 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
+import Joi from 'joi';
 
-const langSchema = new Schema({
-    title: { type: String },
-    title_desc: { type: String },
-    intro_msg: { type: String },
-    intro_note: { type: String },
-    cca_msg: { type: String },
-    intro_desc: { type: String },
-    footer_desc_1: { type: String },
+export const ConfigsSchema = Joi.object({
+    _id: Joi.object().allow(),
+    GLOBAL: Joi.object({
+        en: Joi.object(),
+    }),
 });
 
-const pageSchema = new Schema({
-    en: { type: langSchema },
-});
-
-const configSchema = new Schema(
-    {
-        GLOBAL: { type: pageSchema },
-    },
-    { collection: 'configs' }
-);
-
-export interface Config extends Document {
-    GLOBAL: {
-        en: {
-            title: string;
-            title_desc: string;
-            intro_msg: string;
-            intro_note: string;
-            cca_msg: string;
-            intro_desc: string;
-            footer_desc_1: string;
-        };
-    };
-}
-
-const ConfigSchema =
-    mongoose.models.configs || mongoose.model('configs', configSchema);
-export default ConfigSchema;
+export default ConfigsSchema;
