e6f94b618c83597c457b92e262011b5ea72c15f2
nordic96 <rhrlgns96@gmail.com>
2025-07-14 17:02:49 +0900

    created api to fetch release data

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 const API_PREFIX = '/api';
-export type ApiRoutes = 'projects' | 'configs' | 'graphql';
+export type ApiRoutes = 'projects' | 'configs' | 'graphql' | 'releases';
 
 export function constructApiRoute(route: ApiRoutes): string {
     return path.join(API_PREFIX, route);
