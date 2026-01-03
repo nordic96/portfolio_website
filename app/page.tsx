751a124305c190058f65ccdbb4f7605cf6bf6275
nordic96 <rhrlgns96@gmail.com>
2026-01-03 14:28:11 +0800

    cleanup css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
+'use client';
+
 import HeroSection from '@/components/HeroSection';
+import TechStackLogos from '@/components/HeroSection/TechStackLogos';
+import ProjectSection from '@/components/ProjectSection/ProjectSection';
+import { useBreakpoint } from './hooks';
+import { cn } from './utils';
+import { baseWidth } from './styles';
 
 export default function Home() {
+  const breakpoint = useBreakpoint();
   return (
-    <main className={'min-h-screen'}>
-      <HeroSection />
-      {/* Additional sections will be added in future phases */}
+    <main className={'relative min-h-screen flex flex-col items-center'}>
+      <TechStackLogos
+        animation={true}
+        radius={breakpoint === 'mobile' ? 40 : 30}
+      />
+      <div
+        className={cn(baseWidth, 'flex flex-col justify-center items-center')}
+      >
+        <HeroSection />
+        {/* Additional sections will be added in future phases */}
+        <ProjectSection />
+      </div>
     </main>
   );
 }
