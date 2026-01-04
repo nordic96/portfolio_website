8e862b77514132dd70d0356cf9d6b64d66cd5d51
nordic96 <rhrlgns96@gmail.com>
2026-01-04 19:27:14 +0800

    added test korean locales

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from 'next-intl/routing';
 
-export const LOCALES = ['en'];
+export const LOCALES = ['en', 'ko'];
 export const DEFAULT_LOCALE = 'en';
 
 export const routing = defineRouting({
