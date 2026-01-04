55dc1db5af86743ff1a259330ebcd8e057ebd705
nordic96 <rhrlgns96@gmail.com>
2026-01-04 19:14:54 +0800

    added css for flags

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import '../globals.css';
+import '@/node_modules/flag-icons/css/flag-icons.min.css';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
