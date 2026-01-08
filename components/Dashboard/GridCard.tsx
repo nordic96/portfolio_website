d263092db9e68ee9da3c619be0b4582621c7f095
nordic96 <rhrlgns96@gmail.com>
2026-01-08 14:31:16 +0800

    section title label styling

--- components/Dashboard/GridCard.tsx
+++ components/Dashboard/GridCard.tsx
@@ -39,11 +39,7 @@ export default function GridCard({
 
   return (
     <div className={'flex flex-col grow h-full'}>
-      {title && (
-        <h2 className="text-lg font-normal text-text-dark tracking-wider mb-3">
-          {title}
-        </h2>
-      )}
+      {title && <CardLabel>{title}</CardLabel>}
       <div
         className={cn(
           baseStyles,
@@ -54,6 +50,30 @@ export default function GridCard({
         )}
       >
         {children}
+      </div>
+    </div>
+  );
+}
+
+function CardLabel({ children }: React.PropsWithChildren<object>) {
+  return (
+    <div
+      className={
+        'flex justify-center items-center rounded-lg bg-gray-50 border-gray-400 border shadow-md p-1 max-w-50 max-sm:w-full'
+      }
+    >
+      <div
+        className={
+          'border-2 border-red-300 rounded-md flex grow items-center px-4 max-sm:px-2'
+        }
+      >
+        <h2
+          className={
+            'text-lg max-sm:text-sm font-semibold font-stretch-extra-condensed text-text-dark'
+          }
+        >
+          {children}
+        </h2>
       </div>
     </div>
   );
