ffeb07a99128b5ba1a124abb85ed6863a01371a3
nordic96 <rhrlgns96@gmail.com>
2026-04-14 10:51:52 +0800

    refactored api routes function to fetch isbn data from CDN

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -2,15 +2,26 @@ import { getBookData } from '@/src/lib/openlibrary';
 import { BookApiResponse } from '@/src/types/book';
 import { NextResponse } from 'next/server';
 
+interface ISBNResponse {
+  reading: string[];
+  completed: string[];
+}
+
 export async function GET() {
   try {
-    const isbnArr = ['9780099584575'];
-    const completedIsbnArr = [
-      '9781473695993',
-      '9780132350884',
-      '9789352135240',
-      '9780143124986',
-    ];
+    let isbnArr = [];
+    let completedIsbnArr = [];
+    const url = new URL(
+      `${process.env.NEXT_PUBLIC_CDN_BASE}/resources/books.json`,
+    );
+    const isbnRes = await fetch(url);
+    if (isbnRes.status === 200 && isbnRes.ok) {
+      const data = (await isbnRes.json()) as ISBNResponse;
+      isbnArr = data.reading || [];
+      completedIsbnArr = data.completed || [];
+    } else {
+      throw new Error('failed to fetch isbn data');
+    }
     const promiseArr = isbnArr.map((x) => getBookData(x));
     const completedArr = completedIsbnArr.map((x) => getBookData(x));
 
