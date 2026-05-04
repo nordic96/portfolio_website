14016f4fe1b38f51580c897f344df56a9949444b
nordic96 <rhrlgns96@gmail.com>
2026-05-04 21:38:41 +0800

    added spotify top tracks API * v1/spotify/top-tracks BFF API route * library function to fetch top-tracks * TopTracksAPI Response interface * spotify env config SPOTIFY_API_BASE_URL added * testData added for testing offline data * useSpotifyStore updated with fetchTracks action to fetch and save tracks data

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,10 +1,15 @@
 import {
+  SPOTIFY_API_BASE_URL,
   SPOTIFY_CLIENT_ID,
   SPOTIFY_CLIENT_SECRET,
   SPOTIFY_REFRESH_TOKEN,
   SPOTIFY_TIME_RANGE,
 } from '../config';
-import { SpotifyTokenResponse, TopArtistResponse } from '../types';
+import {
+  SpotifyTokenResponse,
+  TopArtistResponse,
+  TopTracksResponse,
+} from '../types';
 
 const TOKEN_URL = 'https://accounts.spotify.com/api/token';
 
@@ -34,17 +39,54 @@ export async function generateAccessToken(): Promise<SpotifyTokenResponse> {
   throw new Error('Failed to refresh token');
 }
 
-export async function getTopArtists(
-  token: string,
-  limit = 5,
-  locale?: string,
-): Promise<TopArtistResponse> {
-  const url = new URL('https://api.spotify.com/v1/me/top/artists');
+/**
+ * prepares Spotify API Base URL with suffix and searchParams with limit & locale
+ * @param suffix '/me/top/{artists|tracks}
+ * @param limit default 5
+ * @param locale locale string 'SG_EN' 'KO_KR'
+ * @returns appended API URL instance
+ */
+function prepareSpotifyApiUrl(suffix: string, limit = 5, locale?: string): URL {
+  const spotifyApiBaseUrl = SPOTIFY_API_BASE_URL;
+  const url = new URL(suffix, spotifyApiBaseUrl);
   url.searchParams.append('limit', limit.toString());
   url.searchParams.append('time_range', SPOTIFY_TIME_RANGE);
   if (locale) {
     url.searchParams.append('locale', locale);
   }
+  return url;
+}
+
+export async function getTopTracks(
+  token: string,
+  limit = 5,
+  locale?: string,
+): Promise<TopTracksResponse> {
+  const url = prepareSpotifyApiUrl('v1/me/top/tracks', limit, locale);
+  const res = await fetch(url, {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+    next: { revalidate: 3600 },
+  });
+  if (res.status === 401) {
+    throw new Error('Unauthorized Error');
+  } else if (res.status === 403) {
+    throw new Error('Bad OAuth Request');
+  } else if (res.status === 429) {
+    throw new Error('Exceeded Rate Limits');
+  } else if (!res.ok) {
+    throw new Error('Failed to fetch top artist data');
+  }
+  return res.json();
+}
+
+export async function getTopArtists(
+  token: string,
+  limit = 5,
+  locale?: string,
+): Promise<TopArtistResponse> {
+  const url = prepareSpotifyApiUrl('v1/me/top/artists', limit, locale);
   const res = await fetch(url, {
     headers: {
       Authorization: 'Bearer ' + token,
--- src/store/useSpotifyStore.ts
+++ src/store/useSpotifyStore.ts
@@ -1,23 +1,28 @@
 import { create } from 'zustand';
-import { Artist, TopArtistResponse } from '../types';
+import { Artist, TopArtistResponse, TopTracksResponse, Track } from '../types';
 
 export const ARTISTS_TO_FETCH = 20;
+export const TRACKS_TO_FETCH = 8;
 
 type State = {
   loading: boolean;
   artists: Artist[];
+  tracks: Track[];
   error: Error | null;
 };
 
 type Actions = {
   fetchArtists: () => Promise<void>;
+  fetchTracks: () => Promise<void>;
 };
 
 let controller: AbortController | null = null;
+let trackRequestController: AbortController | null = null;
 type SpotifyState = State & Actions;
 export const useSpotifyStore = create<SpotifyState>()((set) => ({
   loading: false,
   artists: [],
+  tracks: [],
   error: null,
   fetchArtists: async () => {
     try {
@@ -36,6 +41,31 @@ export const useSpotifyStore = create<SpotifyState>()((set) => ({
       const data = (await res.json()) as TopArtistResponse;
       if (data.items.length > 0) {
         set(() => ({ artists: data.items }));
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        set(() => ({ error: e }));
+      }
+    } finally {
+      controller = null;
+      set(() => ({ loading: false }));
+    }
+  },
+  fetchTracks: async () => {
+    try {
+      if (trackRequestController !== null) {
+        trackRequestController.abort();
+      }
+      trackRequestController = new AbortController();
+      const res = await fetch('/api/v1/spotify/top-tracks', {
+        signal: trackRequestController.signal,
+      });
+      if (res.status !== 200) {
+        throw new Error(`${res.status}`);
+      }
+      const data = (await res.json()) as TopTracksResponse;
+      if (data.items.length > 0) {
+        set(() => ({ tracks: data.items }));
       }
     } catch (e) {
       if (e instanceof Error) {
